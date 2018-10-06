e23dfc5b10cb534a85ccbfc9e4118743b4ccb032
Fiona Campbell <mail@fionacampbell.net>
2018-10-06 16:32:38 +0100

    Initial Commit

---
+++ MoodTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MoodTracker
+//
+//  Created by Fiona Campbell on 06/10/2018.
+//  Copyright © 2018 Fiona Campbell. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MoodTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MoodTracker
+//
+//  Created by Fiona Campbell on 06/10/2018.
+//  Copyright © 2018 Fiona Campbell. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
