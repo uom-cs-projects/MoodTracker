8d8794ba5341b20cfb49c3ccf6ed0d9c55d3d398
Fiona Campbell <mail@fionacampbell.net>
2018-10-06 18:04:10 +0100

    initial push and button testing

--- MoodTracker/SecondViewController.swift
+++ MoodTracker/SecondViewController.swift
@@ -15,6 +15,15 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var submit: UIButton!
+    @IBOutlet weak var textbox: UITextField!
+    
+    @IBAction func clicksubmit(_ sender: Any) {
+        textbox.text = "test";
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
