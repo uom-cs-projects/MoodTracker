25f0bed129c1358e7c3e5ee2307b632eb8a027d6
Fiona Campbell <mail@fionacampbell.net>
2018-10-08 21:39:33 +0100

    test buttons, pictures and adding view controllers

--- MoodTracker/SecondViewController.swift
+++ MoodTracker/SecondViewController.swift
@@ -8,21 +8,62 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textbox.delegate = self
     }
 
     @IBOutlet weak var submit: UIButton!
     @IBOutlet weak var textbox: UITextField!
-    
+    @IBOutlet weak var picview: UIImageView!
+    @IBAction func tappic(_ sender: UITapGestureRecognizer) {
+        // Hide the keyboard.
+        textbox.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        picview.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
     @IBAction func clicksubmit(_ sender: Any) {
         textbox.text = "test";
         
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textbox.text = textField.text
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
