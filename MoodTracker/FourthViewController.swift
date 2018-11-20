89846dde1f0a9462023a76bf6a3e89da5161166e
Fiona Campbell <mail@fionacampbell.net>
2018-11-20 10:29:14 +0000

    creating new tab pages

---
+++ MoodTracker/FourthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FourthViewController.swift
+//  MoodTracker
+//
+//  Created by Fiona Campbell on 20/11/2018.
+//  Copyright © 2018 Fiona Campbell. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
